cef390f34e8006e2cbfab4e0c5224ad2cbaacbfa
Jeremy Puhlman <jpuhlman@mvista.com>
2018-06-07 00:09:33 +0000

    Add meta-qa

--- setup.sh
+++ setup.sh
@@ -48,6 +48,8 @@ LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-cgl.git;branch=mas
 LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-cloud-services.git;branch=master;layer=meta-openstack \
 LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-cloud-services.git;branch=master \
 LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-montavista-cgl;branch=sumo \
+LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-qa.git;branch=sumo;layer=meta-qa-framework \
+LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-qa.git;branch=sumo;layer=meta-qa-testsuites \
 MACHINE@qemux86-64 \
 DISTRO@mvista-cgx \
 SOURCE@https://github.com/MontaVista-OpenSourceTechnology/linux-mvista-2.4;branch=mvl-4.14/msd.cgx;meta=MV_KERNEL \
